2a64a5ce565b773ea66315b4a6bc25ffe874562b
CacucoH <69527419+CacucoH@users.noreply.github.com>
2021-02-22 01:47:03 +0300

    Add files via upload

--- install.sh
+++ install.sh
@@ -1,18 +1,18 @@
-echo '[!] Updating...'
-apt-get update > install.log
+echo '[i] Updating...'
+apk update > install.log
 echo
-echo '[!] Installing Dependencies...'
-echo '    Python3'
-apt-get -y install python3 python3-pip &>> install.log
-echo '    PHP'
-apt-get -y install php &>> install.log
-echo '    ssh'
-apt-get -y install ssh &>> install.log
-echo '    Requests'
+echo '[i] Installing Dependencies...'
+echo '    Installing Python3'
+apk add python3 setuptools &>> install.log
+echo '    Installing PHP'
+apk add php &>> install.log
+echo '    Installing SSH'
+apk add openssh &>> install.log
+echo '    Installing Requests'
 pip3 install requests &>> install.log
 echo
-echo '[!] Setting Permissions...'
+echo '[i] Setting Permissions...'
 chmod 777 template/nearyou/php/info.txt
 chmod 777 template/nearyou/php/result.txt
 echo
-echo '[!] Installed.'
+echo '[i] Succesfully installed!'
